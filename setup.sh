b4f6ed1af67715a59509e509d64f66c978831b07
Finn Nannestad <finn.nannestad@shinesolutions.com>
2018-11-06 12:37:40 +1100

    Initial implementation of CloudFormation template

--- setup.sh
+++ setup.sh
@@ -8,9 +8,30 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-timestamp=$(date +%s%3)
+# Defaults
+CURRENT_TIME=$(date +%s%3)
+AVAILABILIY_ZONE="ap-southeast-2a"
+PRIVATE_BUCKET="ut4-hub-config"
+AMI="ami-0c24c2b222474a63b"
+VPC=""
+INSTANCE_TYPE="t2.micro"
+CONFIG_BUCKET_NAME="ut4-aws-hub-config-$CURRENT_TIME"
+FILES_BUCKET_NAME="ut4-aws-hub-files-$CURRENT_TIME"
+SUBNET="subnet-0e00ca65cad0745c6"
 
-# Append timestamp to avoid conflicting bucket names
-aws s3api create-bucket --bucket ut4-hub-maps-$timestamp --region $2
-aws s3api create-bucket --bucket ut4-hub-files-$timestamp --region $2
-aws cloudformation create-stack --template-url cloudformation/ut4-aws-hub.json --parameters VPCID=$3 --profile $1 --region $2
+sed -e s/#AVAILABILITY_ZONE#/$AVAILABILITY_ZONE/g \
+    -e s/#CONFIG_BUCKET#/$CONFIG_BUCKET/g \
+    -e s/#AMI#/$AMI/g \
+    -e s/#VPC#/$VPC/g \
+    -e s/#INSTANCE_TYPE#/$INSTANCE_TYPE/g \
+    -e s/#CONFIG_BUCKET_NAME#/$CONFIG_BUCKET_NAME/g \
+    -e s/#FILES_BUCKET_NAME#/$FILES_BUCKET_NAME/g \
+    ut4-aws-hub-params.json > ut4-aws-hub-params-run.json 
+
+# Append CURRENT_TIME to avoid conflicting bucket names
+aws s3api create-bucket --bucket ut4-hub-maps-$CURRENT_TIME --region $2
+aws s3api create-bucket --bucket ut4-hub-files-$CURRENT_TIME --region $2
+aws cloudformation create-stack \
+    --template-body file://cloudformation/ut4-aws-hub.json \
+    --parameters file://cloudformation/ut4-aws-hub-params-run.json \
+    --profile $1 --region $2
